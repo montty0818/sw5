e7e48a7dc8b4d4713d3a454a55bc86210ebe6363
YefSanti <43991345+YefSanti@users.noreply.github.com>
2020-03-06 00:36:46 -0500

    eewd

--- php-api/php-api/database/seeds/Pivots2TableSeeder.php
+++ php-api/php-api/database/seeds/Pivots2TableSeeder.php
@@ -11,6 +11,46 @@ class Pivots2TableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        
+        DB::table('conductor_licencia')->insert([
+        'conductor_id' => '1',
+        'licencia_id' => '1',
+        ]);
+
+        DB::table('conductor_licencia')->insert([
+        'conductor_id' => '1',
+        'licencia_id' => '2',
+        ]);
+
+        DB::table('conductor_licencia')->insert([
+        'conductor_id' => '2',
+        'licencia_id' => '1',
+        ]);
+
+        DB::table('conductor_licencia')->insert([
+        'conductor_id' => '2',
+        'licencia_id' => '3',
+        ]);
+
+        DB::table('conductor_licencia')->insert([
+        'conductor_id' => '2',
+        'licencia_id' => '4',
+        ]);
+
+
+        DB::table('conductor_notificacion')->insert([
+        'conductor_id' => '1',
+        'notificacion_id' => '1',
+        ]);
+
+        DB::table('conductor_notificacion')->insert([
+        'conductor_id' => '1',
+        'notificacion_id' => '2',
+        ]);
+
+        DB::table('conductor_notificacion')->insert([
+        'conductor_id' => '2',
+        'notificacion_id' => '1',
+        ]);
     }
 }
